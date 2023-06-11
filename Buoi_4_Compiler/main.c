c500c8719067200de01e44398d8d927b991c1596
donghung06 <dongvanhung06@gmail.com>
2023-06-11 15:09:26 +0700

    1

---
+++ Buoi_4_Compiler/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>>
+#include "test.c"
+
+#define MAX 10
+
+
+void display(){
+
+    printf("Hello World\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    //This is function
+
+    display();
+    printf("MAX : %d\n", MAX);
+    
+    test();
+    return 0;
+}
+
+
+
+
+
---
+++ Buoi_4_Compiler/main.s
@@ -0,0 +1,77 @@
+	.file	"main.c"
+	.section .rdata,"dr"
+.LC0:
+	.ascii "This is test \0"
+	.text
+	.globl	test
+	.def	test;	.scl	2;	.type	32;	.endef
+	.seh_proc	test
+test:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	leaq	.LC0(%rip), %rcx
+	call	puts
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.section .rdata,"dr"
+.LC1:
+	.ascii "Hello World\0"
+	.text
+	.globl	display
+	.def	display;	.scl	2;	.type	32;	.endef
+	.seh_proc	display
+display:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	leaq	.LC1(%rip), %rcx
+	call	puts
+	nop
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC2:
+	.ascii "MAX : %d\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movq	%rdx, 24(%rbp)
+	call	__main
+	call	display
+	movl	$10, %edx
+	leaq	.LC2(%rip), %rcx
+	call	printf
+	call	test
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (tdm64-1) 4.9.2"
+	.def	puts;	.scl	2;	.type	32;	.endef
+	.def	printf;	.scl	2;	.type	32;	.endef
