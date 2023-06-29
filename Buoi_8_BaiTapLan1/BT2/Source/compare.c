51ae7f00a7a3c9190c4c1ad459a5b180a6b71202
donghung06 <dongvanhung06@gmail.com>
2023-06-29 11:45:06 +0700

    Chinh Sua

---
+++ Buoi_8_BaiTapLan1/BT2/Source/compare.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include "Header/compare.h"
+
+
+
+Compare compareTwoString (const char arr1[], const char arr2[]){
+    uint8_t i = 0;
+    while (arr1 == arr2){
+        if (arr1[i] == '\0')    
+        {
+            Printf("EQUAL\n");
+            return EQUAL;
+        }
+        i++;
+
+        if (arr1[] > arr2[])
+        {
+            Printf("Bigger\n");
+        }
+
+        else    Printf("SMALLER\n");
+
+    }
+}
+
+
+
